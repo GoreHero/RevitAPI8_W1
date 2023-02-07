cc1814f934f3917ca86aba8d510622167178c179
Кирилл Зайцев <moja.orbita@ya.ru>
2023-02-08 01:17:52 +0300

    Добавьте файлы проекта.

---
+++ RevitAPI8_W2/Main.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI8_W2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document; 
+
+            NavisworksExportOptions nwсOptions = new NavisworksExportOptions 
+            {
+                ExportScope = NavisworksExportScope.Model,
+
+                ViewId = doc.ActiveView.Id
+            };
+
+            doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Export.nwc", nwсOptions);
+            
+            TaskDialog.Show("Инфо", "Готово");
+
+            return Result.Succeeded;
+
+        }
+    }
+}
